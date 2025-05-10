f035d09306e5c1dc8f3032574e9712aaf703a49e
Wang Zihao <e0424629@u.nus.edu>
2025-05-10 20:54:23 +0800

    Add files via upload

--- syphilis_calibration/run_mcmc_syphilis_fixed.R
+++ syphilis_calibration/run_mcmc_syphilis_fixed.R
@@ -7,9 +7,9 @@ rstan_options (auto_write = TRUE)
 options(mc.cores = parallel::detectCores(logical = FALSE)) # use all available cores by default when sampling
 
 # time series of MSM syphilis cases
-# cases <- c(324, 365, 488, 346, 305, 197, 159, 187, 318, 308, 525, 496, 623, 573, 605, 381, 317) # main scenario (male incidence minus female incidence)
+cases <- c(324, 365, 488, 346, 305, 197, 159, 187, 318, 308, 525, 496, 623, 573, 605, 381, 317) # main scenario (male incidence minus female incidence)
 # cases <- c(518, 632, 840, 564, 599, 532, 407, 536, 875, 801, 935, 832, 894, 804, 863, 515, 365) # upper bound (all male incidence)
-cases <- c(31, 38, 51, 34, 36, 32, 24, 32, 53, 48, 56, 50, 54, 49, 52, 31, 22) # lower bound (percentage of MSM among male population, assuming syphilis incidence rate is the same among MSM and MSF)
+# cases <- c(31, 38, 51, 34, 36, 32, 24, 32, 53, 48, 56, 50, 54, 49, 52, 31, 22) # lower bound (percentage of MSM among male population, assuming syphilis incidence rate is the same among MSM and MSF)
 
 # Initial population size of MSM in 2004
 N_t0 <- 97189 # applied the percentage of MSM among male in 2019 to male population in 2004
@@ -43,37 +43,6 @@ t <- seq(0, n_years, by = 1)
 t_0 = 0 
 t <- t[-1]
 
-# initial syphilis incidence guess
-# temp = 324
-
-# initial conditions
-# assume that for each group j
-# 10% of incidences at incubation
-# 20% of incidences at primary stage
-# 25% of incidences at secondary stage
-# 30% of incidences at early latent stage
-# 10% of incidences at late latent stage
-# 5% of incidences at teritary stage
-# 5% of incidences at recovery stage
-# U_N_H = (N_t0 - temp) * q_H;
-# I_N_H = 0.1 * temp * q_H;
-# P_N_H = 0.15 * temp * q_H;
-# S_N_H = 0.25 * temp * q_H;
-# E_N_H = 0.3 * temp * q_H;
-# L_N_H = 0.1 * temp * q_H;
-# T_N_H = 0.05 * temp * q_H;
-# R_N_H = 0.05 * temp * q_H;
-# U_N_L = (N_t0 - temp) * q_L;
-# I_N_L = 0.1 * temp * q_L;
-# P_N_L = 0.15 * temp * q_L;
-# S_N_L = 0.25 * temp * q_L;
-# E_N_L = 0.3 * temp * q_L;
-# L_N_L = 0.1 * temp * q_L;
-# T_N_L = 0.05 * temp * q_L;
-# R_N_L = 0.05 * temp * q_L;
-# y0 = c(U_N_H=U_N_H, I_N_H=I_N_H, P_N_H=P_N_H, S_N_H=S_N_H, E_N_H=E_N_H, L_N_H=L_N_H, T_N_H=T_N_H, R_N_H=R_N_H,
-#        U_N_L=U_N_L, I_N_L=I_N_L, P_N_L=P_N_L, S_N_L=S_N_L, E_N_L=E_N_L, L_N_L=L_N_L, T_N_L=T_N_L, R_N_L=R_N_L)
-
 # data for Stan
 data_syphilis <- list(n_years = n_years, ts = t, t_0 = t_0, q_H = q_H, c_H = c_H, c_L = c_L, q_L = q_L, cases = cases, alpha = alpha, N_t0 = N_t0, gamma = gamma, sigma=sigma, psi_S=psi_S, psi_E=psi_E, psi_L=psi_L, psi_T=psi_T, nu=nu, beta_nu=beta_nu)
 
@@ -97,13 +66,17 @@ fit_syphilis_negbin <- sampling(model,
                                 # diagnostic_file = "diagnostics.csv" # for debugging
                                 verbose=TRUE)
 
-saveRDS(fit_syphilis_negbin, file = "fit_results_lower.rds")
-# fit_syphilis_negbin <- readRDS("fit_results_lower.rds")
+# save the rstan results
+# saveRDS(fit_syphilis_negbin, file = "fit_results_main.rds")
+
+# adjust the parameter 'cases' before plotting
+fit_syphilis_negbin <- readRDS("fit_results_main.rds")
 
 # print the data
-# sink("output_main.txt")
-# print(fit_syphilis_negbin)
-# sink()
+options(max.print = 1000000)
+sink("output_main.txt")
+print(fit_syphilis_negbin)
+sink()
 
 # print the mcmc results
 pars=c('beta', 'phi_beta', 'epsilon', 'rho', 'eta_H_init', 'phi_eta', 'omega', 'mu', 'kappa_D')
@@ -113,64 +86,51 @@ print(fit_syphilis_negbin, pars = pars)
 phi_beta_samples <- rstan::extract(fit_syphilis_negbin, pars = "phi_beta")$phi_beta
 mean(phi_beta_samples)
 
-# trace plots to assess mixing of a chain
+# trace plots to assess mixing of a chain, output size (10, 5)
 traceplot(fit_syphilis_negbin, pars = pars)
 
-# marginal posterior densities
+# marginal posterior densities, output size (10, 5)
 stan_dens(fit_syphilis_negbin, pars = pars, separate_chains = TRUE)
 
 # posterior predictive check on number of cases
 smr_pred <- cbind(as.data.frame(summary(
-  fit_syphilis_negbin, pars = "pred_cases", probs = c(0.025, 0.5, 0.975))$summary), t[1:(n_years-1)], cases[1:(n_years-1)])
+  fit_syphilis_negbin, pars = "pred_cases", probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary), t[1:(n_years-1)], cases[1:(n_years-1)])
 colnames(smr_pred) <- make.names(colnames(smr_pred)) # to remove % in the col names
 
-main_data <- data.frame(
-  Year = 2004:2018,
-  Median = smr_pred$X50.[1:(n_years-2)],
-  Lower = smr_pred$X2.5.[1:(n_years-2)],
-  Upper = smr_pred$X97.5.[1:(n_years-2)],
-  Observation = cases[1:(n_years-2)]
+# box plot, output size (8, 6)
+df <- data.frame(
+  group = factor(2004:2018),
+  ymin = smr_pred$X2.5.[1:(n_years-2)],    # minimum
+  lower = smr_pred$X25.[1:(n_years-2)],  # Q1
+  middle = smr_pred$X50.[1:(n_years-2)], # median
+  upper = smr_pred$X75.[1:(n_years-2)],  # Q3
+  ymax = smr_pred$X97.5.[1:(n_years-2)],  # maximum
+  observation = cases[1:(n_years-2)],    # observed
+  year = 2004:2018                       # year
 )
 
-plot_data <- main_data |>
-  pivot_longer(cols = c("Median", "Observation"), names_to = "Type", values_to = "Value")
-
-# Base plot
-ggplot() +
-  # Credible interval
-  geom_linerange(
-    data = main_data,
-    aes(x = Year, ymin = Lower, ymax = Upper, color = "95% Credible Interval"),
-    size = 5, alpha = 0.5
-  ) +
-  # Median (discrete horizontal line)
-  geom_segment(
-    data = main_data,
-    aes(x = Year - 0.15, xend = Year + 0.15, y = Median, yend = Median, color = "Median"),
-    size = 1.2
-  ) +
-  # Observed points
-  geom_point(
-    data = main_data,
-    aes(x = Year, y = Observation, color = "Observed"), shape = 18, size = 2.5
-  ) +
-  scale_color_manual(
-    name = NULL,
-    values = c(
-      "95% Credible Interval" = "salmon",
-      "Median" = "darkred",
-      "Observed" = "deepskyblue4"
-    )
-  ) +
-  labs(
-    title = "Main Scenario: Model Projections vs Observed",
-    y = "Annual incidence of diagnosed",
-    x = "Year"
-  ) +
-  scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0, NA)) +
+ggplot(df, aes(x = group, ymin = lower, lower = lower, middle = middle, upper = upper, ymax = upper, color = 'Predicted')) +
+  geom_boxplot(stat = "identity", fill = "salmon") +
+  geom_point(data = df, aes(x = group, y = observation, color = "Observed"), shape = 18, size = 2.5) +
+  labs(x = "Year", y = "Annual Incidence of Diagnosed") +
+  scale_color_manual(name = NULL, values = c("Predicted" = "darkred", "Observed" = "deepskyblue4")) +
   theme_minimal(base_size = 13) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05)), limits = c(0, NA)) +
   theme(
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
-    legend.position = "top"
+    legend.position = c(0.85, 0.85),
+    legend.justification = c("right", "top"),
+    legend.background = element_rect(fill = alpha("white", 0.6), color = NA),
+    legend.box.background = element_rect(color = "black"),
+    legend.margin = margin(t = 0, r = 0, b = 0, l = 0),
+    legend.box.margin = margin(t = 0, r = 0, b = 0, l = 0),
+    plot.title = element_text(hjust = 0.5, margin = margin(b = 0)),
+    axis.title.x = element_text(size = 13),
+    axis.title.y = element_text(size = 13),
+    axis.text.x = element_text(size = 13),
+    axis.text.y = element_text(size = 13),
+    legend.title = element_text(size = 13),
+    legend.text = element_text(size = 13),
+    plot.margin = margin(t = 0, r = 0, b = 0, l = 0)
   )
